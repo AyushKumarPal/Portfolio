dc8d04ca360c8fd748b8ff3e92c87f53a1969e76
Ayush Kumar Pal <56873383+AyushKumarPal@users.noreply.github.com>
2023-08-22 09:57:30 +0530

    Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ function Header() {
     return (
         <nav>
             <ul className='left'>
-                <li><img src="https://i.pinimg.com/originals/c9/12/d4/c912d49f5f63e3c25aae2465f7577e7a.png" alt="logo" /></li>
+                {/* <li><img src="https://i.pinimg.com/originals/c9/12/d4/c912d49f5f63e3c25aae2465f7577e7a.png" alt="logo" /></li> */}
             </ul>
             <ul className='right'>
-                <li><Link to='/home'>Home</Link></li>
+                <li><Link to='/'>Home</Link></li>
                 <li><Link to='/projects'>Projects</Link></li>
                 <li><Link to='/contact'>Contact Me</Link></li>
             </ul>
