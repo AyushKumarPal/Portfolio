b653a9350739f04c49dbe7aac6b140f6f6d659c7
Ayush Kumar Pal <56873383+AyushKumarPal@users.noreply.github.com>
2023-08-22 09:55:22 +0530

    Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,19 @@ import EmailIcon from '@mui/icons-material/Email';
 function Footer() {
   return (
     <div class="bottom-container">
-        <a class="footer-link" href="https://www.linkedin.com/in/ayush-kumar-pal/">LinkedIn <LinkedInIcon/></a>
-        <a class="footer-link" href="https://github.com/AyushKumarPal">GitHub <GitHubIcon /></a>
-        <a class="footer-link" href="mailto:ayushkumar71430@gmail.com">Mail<EmailIcon/></a>
-        <p class="copyright">© 2023 Ayush Kumar Pal. </p>
+      <div className='fmain'>
+        <div>
+          <a class="footer-link" href="https://www.linkedin.com/in/ayush-kumar-pal/">LinkedIn <LinkedInIcon /></a>   
+        </div>
+        <div>
+          <a class="footer-link" href="https://github.com/AyushKumarPal">GitHub <GitHubIcon /></a>
+        </div>
+        <div>
+          <a class="footer-link" href="mailto:ayushkumar71430@gmail.com">Mail <EmailIcon /></a>
+        </div>
       </div>
+      <p class="copyright">© 2022 Ayush Kumar Pal. </p>
+    </div>
 
   )
 }
